bd2779b4ec7cde5545c17ba9c114779f5c062437
anoopreddykunta <anoopreddy240@gmail.com>
2025-03-25 02:45:42 -0700

    Adding @ID

--- FileCloud/src/main/java/com/FileCloud/FileCloud/entity/UploadFileResponse.java
+++ FileCloud/src/main/java/com/FileCloud/FileCloud/entity/UploadFileResponse.java
@@ -1,15 +1,22 @@
 package com.FileCloud.FileCloud.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class UploadFileResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private boolean isError;
     private String fileName;
     private String fileLink;
