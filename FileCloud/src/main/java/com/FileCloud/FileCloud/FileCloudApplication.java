3e2da73efa0c6de5246acafe1b49945878fe9285
anoopreddykunta <anoopreddy240@gmail.com>
2025-03-25 04:01:36 -0700

    Config changes

--- FileCloud/src/main/java/com/FileCloud/FileCloud/FileCloudApplication.java
+++ FileCloud/src/main/java/com/FileCloud/FileCloud/FileCloudApplication.java
@@ -3,7 +3,7 @@ package com.FileCloud.FileCloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EntityScan("com.FileCloud.FileCloud.entity")
 @SpringBootApplication
 public class FileCloudApplication {
 
