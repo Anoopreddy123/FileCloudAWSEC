e9c2dab6c9541c3920e42b574967995f89929968
anoopreddykunta <anoopreddy240@gmail.com>
2025-03-25 11:48:44 -0700

    Adding the CORS policy

--- src/main/java/com/FileCloud/FileCloud/config/SecurityConfig.java
+++ src/main/java/com/FileCloud/FileCloud/config/SecurityConfig.java
@@ -85,7 +85,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Adjust as per your frontend URL
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173",
+                "http://192.168.4.166:5173")); // Adjust as per your frontend URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
