8f95584cc8c4ea8f5fd87f62821674038900b0d5
anoopreddykunta <anoopreddy240@gmail.com>
2025-03-25 02:35:27 -0700

    Adding endpoint for validation

--- FileCloud/src/main/java/com/FileCloud/FileCloud/entity/UploadFileResponse.java
+++ FileCloud/src/main/java/com/FileCloud/FileCloud/entity/UploadFileResponse.java
@@ -1,16 +1,16 @@
 package com.FileCloud.FileCloud.entity;
 
-import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UploadFileResponse {
-
     private boolean isError;
     private String fileName;
     private String fileLink;
-
-
 }
