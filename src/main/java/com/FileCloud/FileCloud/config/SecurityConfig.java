c2e59bbe7f509e5583daee49de09967749d95719
anoopreddykunta <anoopreddy240@gmail.com>
2025-03-25 18:52:48 -0700

    Adding the CORS config

--- src/main/java/com/FileCloud/FileCloud/config/SecurityConfig.java
+++ src/main/java/com/FileCloud/FileCloud/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173",
-            "http://192.168.4.166:5173")); // Adjust as per your frontend URL
+            "http://192.168.4.166:5173" , "https://filecloud-xi.vercel.app/")); // Adjust as per your frontend URL
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
     configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
@@ -73,7 +73,7 @@ public CorsConfigurationSource corsConfigurationSource() {
         http
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://192.168.4.166:5173","https://filecloud-xi.vercel.app"));
+                    config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://192.168.4.166:5173", "https://filecloud-xi.vercel.app"));
                     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
                     config.setExposedHeaders(Arrays.asList("x-auth-token"));
